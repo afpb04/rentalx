67ec691f5a06df8974dd4cb86fe1f21c3c996a63
Antonio Francisco <afpb04@hotmail.com>
2021-04-12 19:24:37 -0300

    Rental Devolution

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -11,24 +11,38 @@ class RentalsRepository implements IRentalsRepository {
     this.repository = getRepository(Rental);
   }
   async create({
+    id,
     user_id,
     car_id,
     expected_return_date,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
+      id,
       user_id,
       car_id,
       expected_return_date,
+      end_date,
+      total,
     });
     await this.repository.save(rental);
     return rental;
   }
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const rental = await this.repository.findOne({ car_id });
+    const rental = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return rental;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const rental = await this.repository.findOne({ user_id });
+    const rental = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
+    return rental;
+  }
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
     return rental;
   }
 }
