2920a13bf309c8001a566d27cd01daba2b146f41
Antonio Francisco <afpb04@hotmail.com>
2021-04-20 19:49:06 -0300

    fix authenticate

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,10 @@ interface IPayLoad {
   sub: string;
   email: string;
 }
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -18,7 +22,7 @@ class RefreshTokenUseCase {
     @inject('DayJsDateProvider')
     private dateProvider: IDateProvider,
   ) {}
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub } = verify(token, auth.secret_refresh_token) as IPayLoad;
 
     const user_id = sub;
@@ -48,7 +52,14 @@ class RefreshTokenUseCase {
       refresh_token,
       user_id,
     });
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+    return {
+      refresh_token,
+      token: newToken,
+    };
   }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import auth from '@config/auth';
-import UsersTokensRepository from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import UsersRepository from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import AppError from '@shared/errors/AppError';
 
 interface IPayload {
@@ -16,22 +16,15 @@ export default async function ensureAuthenticated(
 ): Promise<void> {
   const authHeader = request.headers.authorization;
 
-  const usersTokensRepository = new UsersTokensRepository();
-
   if (!authHeader) {
     throw new AppError('Token missing!', 401);
   }
   const [, token] = authHeader.split(' ');
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token,
-    ) as IPayload;
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token,
-    );
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User does not exists!', 401);
